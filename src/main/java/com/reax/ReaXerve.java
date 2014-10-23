fc3ebaf8a63abdfee972847ac4b901b506cbee6f
RuedigerMoeller <moru0011@gmail.com>
2014-10-23 17:38:29 +0200

    .

--- src/main/java/com/reax/ReaXerve.java
+++ src/main/java/com/reax/ReaXerve.java
@@ -1,9 +1,104 @@
 package com.reax;
 
 import org.nustaq.kontraktor.Actor;
+import org.nustaq.kontraktor.Actors;
+import org.nustaq.kontraktor.Future;
+import org.nustaq.kontraktor.Promise;
+import org.nustaq.kontraktor.impl.ElasticScheduler;
+import org.nustaq.kontraktor.remoting.RemoteRefRegistry;
+import org.nustaq.kontraktor.remoting.http.netty.wsocket.WSocketActorServer;
+import org.nustaq.kontraktor.remoting.tcp.TCPActorServer;
+import org.nustaq.netty2go.NettyWSHttpServer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ruedi on 23.10.2014.
  */
 public class ReaXerve extends Actor<ReaXerve> {
+
+    Map<String,ReaXession> sessions;
+    long sessionIdCounter = 1;
+
+    ElasticScheduler clientScheduler;
+
+    public void $init( int numClientThreads ) {
+        sessions = new HashMap<>();
+        clientScheduler = new ElasticScheduler(numClientThreads,5000);
+    }
+
+    /**
+     * to avoid the need for anonymous clients to already create a websocket connection,
+     * this is exposed as a webservice and is called using $.get(). The Id returned then can be
+     * used to obtain a valid session id for the websocket connection.
+     *
+     * @param user
+     * @param pwd
+     * @return
+     */
+    public Future<String> $authenticate( String user, String pwd ) {
+        if ( user != null && pwd != null ) // dummy auth
+        {
+            ReaXession newSession = Actors.AsActor(ReaXession.class, clientScheduler);
+            String sessionId = "" + sessionIdCounter++; // can be more cryptic in the future
+            newSession.$init(sessionId, user, self());
+            sessions.put(sessionId, newSession);
+            return new Promise<>(sessionId,null);
+        }
+        return new Promise<>(null,"authentication failure");
+    }
+
+    public Future<ReaXession> $getSession(String id) {
+        return new Promise<>(sessions.get(id));
+    }
+
+    public void $clientTerminated(ReaXession session) {
+        session.$getId().then((id, err) -> sessions.remove(id));
+    }
+
+
+    /**
+     * startup server
+     * @param arg
+     * @throws Exception
+     */
+
+    public static void main( String arg[] ) throws Exception {
+        if ( arg.length > 1 ) {
+            System.out.println("Expect port as first argument");
+            System.exit(1);
+        }
+        int port = 7777;
+        if ( arg.length > 0 ) {
+            try {
+                port = Integer.parseInt(arg[0]);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("Expect port as first argument");
+                System.exit(1);
+            }
+        }
+
+        ReaXerve act = Actors.AsActor(ReaXerve.class);
+        act.$init(2);
+
+        WSocketActorServer server = new WSocketActorServer( act, new File("./"), RemoteRefRegistry.Coding.MinBin );
+//        server.setFileMapper( (f) -> {
+//            if ( f != null && f.getName() != null ) {
+//                if ( f.getName().equals("minbin.js") ) {
+//                    File file = new File("C:\\work\\GitHub\\fast-serialization\\src\\main\\javascript\\minbin.js");
+//                    if ( ! file.exists() ) {
+//                        return new File("/home/ruedi/IdeaProjects/fast-serialization/src/main/javascript/minbin.js");
+//                    }
+//                    return file;
+//                }
+//            }
+//            return f;
+//        });
+        new NettyWSHttpServer(port, server).run();
+    }
+
 }
--- src/main/java/com/reax/ReaXession.java
+++ src/main/java/com/reax/ReaXession.java
@@ -1,9 +1,23 @@
 package com.reax;
 
 import org.nustaq.kontraktor.Actor;
+import org.nustaq.kontraktor.Future;
+import org.nustaq.kontraktor.Promise;
 
 /**
  * Created by ruedi on 23.10.2014.
  */
 public class ReaXession extends Actor<ReaXession> {
+
+    long creationTime = System.currentTimeMillis();
+    long lastHB = creationTime;
+    String sessionId;
+
+    public void $init(String sessionId, String user, ReaXerve self) {
+        this.sessionId = sessionId;
+    }
+
+    public Future<String> $getId() {
+        return new Promise<>(sessionId);
+    }
 }
