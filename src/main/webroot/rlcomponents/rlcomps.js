ebeaded0a9ab324e3183686ea70ee4e9b0b43181
RuedigerMoeller <moru0011@gmail.com>
2014-11-01 01:13:20 +0100

    mach2

--- src/main/webroot/rlcomponents/rlcomps.js
+++ src/main/webroot/rlcomponents/rlcomps.js
@@ -44,12 +44,72 @@ ko.bindingHandlers.hilight = {
     }
 };
 
-// <div data-bind='rlrecord: { table: '', recordId: '' }'
+// <div data-bind='rlrecord: { table: '', key: '' }'
 ko.bindingHandlers.rlrecord = {
     init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+        var functionsOnUnsubscribe = [];
+        var self = this;
+        var currentParams = valueAccessor();
+        for ( var i = 0; i < functionsOnUnsubscribe.length; i++ ) {
+            functionsOnUnsubscribe[i].apply(self);
+        }
+
+        var table = currentParams.table;
+        if ( ko.isObservable(table) ) {
+            table.subscribe(function(newValue) {
+
+            });
+            table = currentParams.table();
+        }
+
+        var key = currentParams.key;
+        if ( ko.isObservable(key) ) {
+            key.subscribe(function(newKey) {
+
+            });
+            key = currentParams.table();
+        }
+        var recordObj = ko.observable({recordKey:null});
+
+        Server.doOnceLoggedIn( function() {
+            Server.session().$subscribeKey(table, key, function( change,err) {
+                switch (change.type) {
+                    case RL_ADD:
+                    {
+                        recordObj(change.newRecord);
+                    }
+                    break;
+                    case RL_REMOVE:
+                    {
+                        recordObj(null);
+                    }
+                    break;
+                    case RL_SNAPSHOT_DONE:
+                        //$scope.snapFin = true;
+                        break;
+                    case RL_UPDATE:
+                    {
+                        var rec = recordObj();
+                        if (rec) {
+                            var changeArray = change.appliedChange.fieldIndex;
+                            for (var i = 0; i < changeArray.length; i++) {
+                                var fieldId = changeArray[i];
+                                var newValue = change.appliedChange.newVal[i];
+                                var fieldName = RealLive.getFieldName(change.tableId, fieldId);
+                                rec[fieldName] = newValue;
+                            }
+                            recordObj( JSON.parse(JSON.stringify(rec)) );
+                        }
+                    }
+                    break;
+                }
+            });
+        });
+
         // Make a modified binding context, with a extra properties, and apply it to descendant elements
         var childBindingContext = bindingContext.createChildContext(
-            { test: 'Hallo'},
+            ko.observable(recordObj),
             null, // Optionally, pass a string here as an alias for the data item in descendant contexts
             function(context) {
                 ko.utils.extend(context, valueAccessor());
